6fc40c92090e4e64ab54b1c26fae5266a532b195
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-02-07 00:31:09 +0700

    ADD : Listing 06

---
+++ Listing_06/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+/// <summary>
+/// Getting a result with the Task Factory
+/// </summary>
+namespace Listing_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create the task
+            Task<int> task1 = Task.Factory.StartNew<int>(() =>
+            {
+                int sum = 0;
+                for (int i = 0; i < 100; i++)
+                {
+                    sum += i;
+                }
+                return sum;
+            });
+
+            // write out the result
+            Console.WriteLine("Result 1: {0}", task1.Result);
+
+            // wait for input before exiting
+            Console.WriteLine("Main method complete. Press enter to finish.");
+            Console.ReadLine();
+        }
+    }
+}
